3c371e19e1bceb00a5f09d7e621da8326180c6b7
JiaaaweiL <105753418+JiaaaweiL@users.noreply.github.com>
2024-09-20 22:36:41 -0500

    Update code.sv

--- 牛客网/VL32 非整数倍数据位宽转换24to128/code.sv
+++ 牛客网/VL32 非整数倍数据位宽转换24to128/code.sv
@@ -1,1 +1,64 @@
-123
+`timescale 1ns/1ns
+
+module width_24to128(
+	input 				clk 		,   
+	input 				rst_n		,
+	input				valid_in	,
+	input	[23:0]		data_in		,
+ 
+ 	output	reg			valid_out	,
+	output  reg [127:0]	data_out
+);
+
+reg	[127:0]	data_lock;
+reg	[3:0] 	cnt;
+
+always @(posedge clk or negedge rst_n) begin
+	if(!rst_n) begin
+		cnt <= 4'b0;
+	end else begin
+		if(valid_in) begin
+			cnt <= cnt + 4'b1;
+		end
+	end
+end
+
+always @(posedge clk or negedge rst_n) begin
+	if(!rst_n) begin
+		data_lock <= 'd0;
+	end else begin
+		if(valid_in) begin
+			data_lock <= {data_lock[103 : 0], data_in};
+		end
+	end
+end
+
+always @(posedge clk or negedge rst_n) begin
+	if(!rst_n) begin
+		valid_out <= 1'b0;	
+	end else begin
+		if(valid_in && (cnt == 4'd5|| cnt == 4'd10|| cnt == 4'd15)) begin
+			valid_out <= 1'b1;
+		end else begin
+			valid_out <= 1'b0;
+		end
+	end
+end
+
+always @(posedge clk or negedge rst_n) begin
+	if(!rst_n) begin
+		data_out <= 128'b0;
+	end else begin
+		if(valid_in && cnt == 4'd5) begin
+			data_out <= {data_lock[119 : 0], data_in[23:16]};
+		end else if (valid_in && cnt == 4'd10)begin
+			data_out <= {data_lock[111 : 0], data_in[23:8]};
+		end else if (valid_in && cnt == 4'd15) begin
+			data_out <= {data_lock[119 : 0], data_in};
+		end
+	end
+end
+
+
+
+endmodule
